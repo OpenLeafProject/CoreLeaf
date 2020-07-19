d4613c8bd87a057241d221cfefe5d83f973304f0
Alex Latorre <alex.latorre@ginsa.cat>
2020-07-19 23:09:31 +0200

    coreController route fix + Newtonsoft.Json to return database version

--- Controllers/coreController.cs
+++ Controllers/coreController.cs
@@ -5,26 +5,45 @@ using Microsoft.Extensions.Logging;
 
 namespace Leaf.Controllers
 {
-    [Route("api/v0.1/[controller]/")]
+    [Route("api/[controller]/")]
     [ApiController]
     public class coreController : ControllerBase
     {
         private readonly ILogger<coreController> _logger;
         private readonly IConfiguration _config;
 
+        /// <summary>
+        ///     Class Contructor
+        /// </summary>
+        /// <param name="logger"> Logguer object for debug </param>
+        /// <param name="config"> appsettings.json properties </param>
         public coreController(ILogger<coreController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
         }
 
+        /// <summary>
+        ///     Returns database version
+        ///     No JWT validation is required
+        /// </summary>
+        /// <returns>
+        ///     Returns database version in JSON Format
+        /// </returns>
         [HttpGet]
         [Route("getdatabaseversion")]
         public String GetDataBaseVersion()
         {
-            return _config.GetValue<string>("Versions:DataBaseVersion");
+           return Newtonsoft.Json.JsonConvert.SerializeObject(_config.GetValue<string>("Versions:DataBaseVersion"));
         }
 
+        /// <summary>
+        ///     Returns api version
+        ///     No JWT validation is required
+        /// </summary>
+        /// <returns>
+        ///     Returns api version in JSON Format
+        /// </returns>
         [HttpGet]
         [Route("getapiversion")]
         public String GetApiVersion()
