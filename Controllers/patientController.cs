36d3bfb2c29a91fcd5e13f3a9ee2d1c4ed68ec5d
Alex Latorre <alex.latorre@ginsa.cat>
2020-07-19 23:13:18 +0200

    Updated Test Function + JWT check enabled

--- Controllers/patientController.cs
+++ Controllers/patientController.cs
@@ -8,29 +8,37 @@ namespace Leaf.Controllers
 {
     [Route("api/v0.1/[controller]")]
     [ApiController]
-    // Security JWT check disabled
-    // [Leaf.Attributes.HeaderCheckJWT]
+    // Enables JWT Auth Security for all methods in controller
+    [Leaf.Attributes.HeaderCheckJWT]
     public class patientController : ControllerBase
     {
         private readonly ILogger<patientController> _logger;
         private readonly IConfiguration _config;
 
+        /// <summary>
+        ///     Class Contructor
+        /// </summary>
+        /// <param name="logger"> Logguer object for debug </param>
+        /// <param name="config"> appsettings.json properties </param>
         public patientController(ILogger<patientController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
         }
 
+        /// <summary>
+        ///     Test method to check DB connection
+        /// </summary>
+        /// <returns>
+        ///     Returns api version in JSON Format
+        /// </returns>
         [HttpGet]
         [Route("test")]
         public System.Data.DataTable Test()
         {
-            // Random rng = new Random();
-            ArrayList myRecipes = new ArrayList();
-
             using (Leaf.Datalayers.Patient.DataLayer dl = new Leaf.Datalayers.Patient.DataLayer(_config))
             {
-                return dl.GetPatients("1");
+                return dl.Test("1");
 
             }
         }
