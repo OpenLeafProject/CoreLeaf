6d2affb14b3f678de6a98a5ee874bf118da78b8b
Alex Latorre <alex.latorre@ginsa.cat>
2020-07-21 15:19:27 +0200

    Working Patient model
    Patient model with load, creation and save methods + datalayer

--- Datalayer/patient.datalayer.cs
+++ Datalayer/patient.datalayer.cs
@@ -14,7 +14,7 @@ namespace Leaf.Datalayers.Patient
 
         }
 
-        public DataTable Test(string id)
+        internal DataTable Test(string id)
         {
             try
             {
@@ -26,6 +26,115 @@ namespace Leaf.Datalayers.Patient
             {
                 Debug.WriteLine(ex.Message);
                 return null;
+            }
+        }
+
+        internal DataTable GetpatientByNHC(int nhc)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM patients WHERE NHC = @NHC";
+
+                return GetDataTable(sql, new Parameters("NHC", DbType.Int32, nhc));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal DataTable GetpatientByDNI(string dni)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM patients WHERE DNI = @DNI";
+
+                return GetDataTable(sql, new Parameters("DNI", DbType.String, dni));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal int createPatient(Models.Patient patient)
+        {
+
+                string sql = @"INSERT INTO `patients` 
+                                (`name`, `surname`, `lastname`, 
+                                `dni`, `phone`, `phonealt`, `address`, 
+                                `city`, `pc`, `sex`, `note`, `lastaccess`, 
+                                `email`, `centerid`, `borndate`) 
+                               VALUES
+	                           (@NAME, @SURNAME, @LASTNAME, @DNI, @PHONE, @PHONEALT, @ADDRESSS, @CITY, 
+                                @PC, @SEX, @NOTE, @LASTACCESS, @EMAIL, @CENTERID, @BORNDATE)";
+
+                return Execute(sql, new Parameters("NAME", DbType.String, patient.Name)
+                                       , new Parameters("SURNAME", DbType.String, patient.Surname)
+                                       , new Parameters("LASTNAME", DbType.String, patient.Lastname)
+                                       , new Parameters("DNI", DbType.String, patient.Dni)
+                                       , new Parameters("PHONE", DbType.String, patient.Phone)
+                                       , new Parameters("PHONEALT", DbType.String, patient.PhoneAlt)
+                                       , new Parameters("ADDRESSS", DbType.String, patient.Address)
+                                       , new Parameters("CITY", DbType.String, patient.City)
+                                       , new Parameters("PC", DbType.Int32, patient.Pc)
+                                       , new Parameters("SEX", DbType.Int32, patient.Sex)
+                                       , new Parameters("NOTE", DbType.String, patient.Note)
+                                       , new Parameters("LASTACCESS", DbType.Int32, patient.LastAccess)
+                                       , new Parameters("EMAIL", DbType.Int32, patient.Email)
+                                       , new Parameters("CENTERID", DbType.Int32, patient.Center.Id)
+                                       , new Parameters("BORNDATE", DbType.Int32, patient.BornDate)
+                                       );
+
+        }
+
+        internal int savePatient(Models.Patient patient)
+        {
+            try
+            {
+                string sql = @"UPDATE `patients` 
+                               SET 
+                                `name` = @NAME, 
+                                `surname` = @SURNAME, 
+                                `lastname` = @LASTNAME, 
+                                `dni` = @DNI, 
+                                `phone` = @PHONE, 
+                                `phonealt` = @PHONEALT, 
+                                `address` = @ADDRESSS, 
+                                `city` = @CITY, 
+                                `pc` = @PC, 
+                                `sex` = @SEX, 
+                                `note` = @NOTE, 
+                                `lastaccess` = @LASTACCESS, 
+                                `email` = @EMAIL, 
+                                `centerid` = @CENTERID, 
+                                `borndate` = @BORNDATE
+                               WHERE `nhc` = @NHC";
+
+                return Execute(sql, new Parameters("NHC", DbType.Int32, patient.Name)
+                                       , new Parameters("NAME", DbType.String, patient.Name)
+                                       , new Parameters("SURNAME", DbType.String, patient.Surname)
+                                       , new Parameters("LASTNAME", DbType.String, patient.Lastname)
+                                       , new Parameters("DNI", DbType.String, patient.Dni)
+                                       , new Parameters("PHONE", DbType.String, patient.Phone)
+                                       , new Parameters("PHONEALT", DbType.String, patient.PhoneAlt)
+                                       , new Parameters("ADDRESSS", DbType.String, patient.Address)
+                                       , new Parameters("CITY", DbType.String, patient.City)
+                                       , new Parameters("PC", DbType.Int32, patient.Pc)
+                                       , new Parameters("SEX", DbType.Int32, patient.Sex)
+                                       , new Parameters("NOTE", DbType.String, patient.Note)
+                                       , new Parameters("LASTACCESS", DbType.Int32, patient.LastAccess)
+                                       , new Parameters("EMAIL", DbType.Int32, patient.Email)
+                                       , new Parameters("CENTERID", DbType.Int32, patient.Center.Id)
+                                       , new Parameters("BORNDATE", DbType.Int32, patient.BornDate)
+                                       );
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return 0;
             }
         }
     }
--- Models/patient.model.cs
+++ Models/patient.model.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,7 +16,7 @@ namespace Leaf.Models
         private string lastname;
         private string dni;
         private string phone;
-        private string pgonealt;
+        private string phonealt;
         private string address;
         private string city;
         private string pc;
@@ -23,6 +25,9 @@ namespace Leaf.Models
         private DateTime lastAccess;
         private string email;
         private Center center;
+        private DateTime bornDate;
+
+        private readonly IConfiguration _config;
 
         public int Nhc { get => nhc; }
         public string Name { get => name; set => name = value; }
@@ -30,7 +35,7 @@ namespace Leaf.Models
         public string Lastname { get => lastname; set => lastname = value; }
         public string Dni { get => dni; set => dni = value; }
         public string Phone { get => phone; set => phone = value; }
-        public string Pgonealt { get => pgonealt; set => pgonealt = value; }
+        public string PhoneAlt { get => phonealt; set => phonealt = value; }
         public string Address { get => address; set => address = value; }
         public string City { get => city; set => city = value; }
         public string Pc { get => pc; set => pc = value; }
@@ -38,26 +43,172 @@ namespace Leaf.Models
         public string Note { get => note; set => note = value; }
         public DateTime LastAccess { get => lastAccess; set => lastAccess = value; }
         public string Email { get => email; set => email = value; }
+        public DateTime BornDate { get => bornDate; set => bornDate = value; }
         public Center Center { get => center; set => center = value; }
 
-        public Patient(int nhc)
+        public Patient(IConfiguration config)
         {
+            _config = config;
+        }
+
+        public Patient(int nhc, IConfiguration config)
+        {
+            _config = config;
             loadPatientbyNHC(nhc);
         }
 
-        public Patient(string dni)
+        public Patient(string dni, IConfiguration config)
         {
+            _config = config;
             loadPatientbyDNI(dni);
+        }
+
+        public Patient(Dictionary<string, string> values, IConfiguration config)
+        {
+            _config = config;
+
+            try
+            {
+                this.nhc = Int32.Parse(values["nhc"]);
+                loadPatientbyNHC(this.Nhc);
+            } catch (Exception ex) {
+                Debug.WriteLine(ex.Message);
+                this.nhc = -1;
+            }
+
+            Name       = values["name"];
+            Surname    = values["surname"];
+            Lastname   = values["lastname"];
+            Dni        = values["dni"];
+            Phone      = values["phone"];
+            PhoneAlt   = values["phoneAlt"];
+            Address    = values["address"];
+            City       = values["city"];
+            Pc         = values["pc"];
+            Sex        = Int32.Parse(values["sex"]);
+            Note       = values["note"];
+            LastAccess = DateTime.Parse(values["lastAccess"]);
+            Email      = values["email"];
+            Center     = new Center(Int32.Parse(values["center"]), _config);
+            BornDate   = DateTime.Parse(values["bornDate"]);
+
         }
 
         private void loadPatientbyNHC(int nhc)
         {
-
+            using (Leaf.Datalayers.Patient.DataLayer dl = new Leaf.Datalayers.Patient.DataLayer(_config))
+            {
+               System.Data.DataTable dt =  dl.GetpatientByNHC(nhc);
+                if(dt.Rows.Count == 1)
+                {
+                    this.nhc = Int32.Parse(dt.Rows[0]["nhc"].ToString());
+                    Name = dt.Rows[0]["name"].ToString();
+                    Surname = dt.Rows[0]["surname"].ToString();
+                    Lastname = dt.Rows[0]["Lastname"].ToString();
+                    Dni = dt.Rows[0]["Dni"].ToString();
+                    Phone = dt.Rows[0]["Phone"].ToString();
+                    PhoneAlt = dt.Rows[0]["PhoneAlt"].ToString();
+                    Address = dt.Rows[0]["Address"].ToString();
+                    City = dt.Rows[0]["City"].ToString();
+                    Pc = dt.Rows[0]["Pc"].ToString();
+                    Sex = Int32.Parse(dt.Rows[0]["Sex"].ToString());
+                    Note = dt.Rows[0]["Note"].ToString();
+                    LastAccess = DateTime.Parse(dt.Rows[0]["LastAccess"].ToString());
+                    Email = dt.Rows[0]["Email"].ToString();
+                    Center = new Center(Int32.Parse(dt.Rows[0]["Centerid"].ToString()), _config);
+                    BornDate = DateTime.Parse(dt.Rows[0]["borndate"].ToString());
+                }
+                else
+                {
+                    throw new System.NullReferenceException("Patient not found");
+                }
+            }
         }
 
         private void loadPatientbyDNI(string dni)
         {
+            using (Leaf.Datalayers.Patient.DataLayer dl = new Leaf.Datalayers.Patient.DataLayer(_config))
+            {
+                System.Data.DataTable dt = dl.GetpatientByDNI(dni);
+                if (dt.Rows.Count == 1)
+                {
+                    this.nhc = Int32.Parse(dt.Rows[0]["nhc"].ToString());
+                    Name = dt.Rows[0]["name"].ToString();
+                    Surname = dt.Rows[0]["surname"].ToString();
+                    Lastname = dt.Rows[0]["Lastname"].ToString();
+                    Dni = dt.Rows[0]["Dni"].ToString();
+                    Phone = dt.Rows[0]["Phone"].ToString();
+                    PhoneAlt = dt.Rows[0]["PhoneAlt"].ToString();
+                    Address = dt.Rows[0]["Address"].ToString();
+                    City = dt.Rows[0]["City"].ToString();
+                    Pc = dt.Rows[0]["Pc"].ToString();
+                    Sex = Int32.Parse(dt.Rows[0]["Sex"].ToString());
+                    Note = dt.Rows[0]["Note"].ToString();
+                    LastAccess = DateTime.Parse(dt.Rows[0]["LastAccess"].ToString());
+                    Email = dt.Rows[0]["Email"].ToString();
+                    Center = new Center(Int32.Parse(dt.Rows[0]["Centerid"].ToString()), _config);
+                    BornDate = DateTime.Parse(dt.Rows[0]["borndate"].ToString());
+                }
+                else
+                {
+                    throw new System.NullReferenceException("Patient not found");
+                }
+            }
+        }
 
+        public Patient createPatient()
+        {
+            using (Leaf.Datalayers.Patient.DataLayer dl = new Leaf.Datalayers.Patient.DataLayer(_config))
+            {
+
+                if(this.Nhc > 0)
+                {
+                    throw new System.IO.InvalidDataException("Cannot create patient with setted NHC");
+                }
+
+                bool correctdni = false ;
+                if(Dni == "" || Dni == null)
+                {
+                    Random rand = new Random();
+                    this.Dni = rand.Next().ToString();
+                    correctdni = true;
+                }
+
+                if(dl.createPatient(this) == 1)
+                {
+                    loadPatientbyDNI(this.dni);
+                    if(correctdni)
+                    {
+                        this.Dni = "";
+                        savePatient();
+                    }
+                    return this;
+                } else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public Patient savePatient()
+        {
+            if (!(this.Nhc > 0))
+            {
+                throw new System.IO.InvalidDataException("Cannot save patient without setted NHC");
+            }
+
+            using (Leaf.Datalayers.Patient.DataLayer dl = new Leaf.Datalayers.Patient.DataLayer(_config))
+            {
+                if (dl.savePatient(this) == 1)
+                {
+                    loadPatientbyNHC(this.nhc);
+                    return this;
+                }
+                else
+                {
+                    return null;
+                }
+            }
         }
     }
 }
