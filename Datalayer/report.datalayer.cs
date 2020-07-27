8e488f7ae3aa09680cfac2a1fb64e47ee5d90492
Alex Latorre <alex.latorre@ginsa.cat>
2020-07-27 14:39:58 +0200

    Report working model
    TODO: Controller

---
+++ Datalayer/report.datalayer.cs
@@ -0,0 +1,98 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Diagnostics;
+
+namespace Leaf.Datalayers.Report
+{
+    public class DataLayer : Leaf.Data.DataLayerBaseMySQL
+    {
+
+        public DataLayer(IConfiguration _config)
+            : base(new Data.ConnectionDB(_config).getConnectString())
+        {
+
+        }
+
+        internal DataTable loadById(int id)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM reports WHERE ID = @ID";
+
+                return GetDataTable(sql, new Parameters("ID", DbType.Int32, id));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal DataTable loadByHash(string hash)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM reports WHERE HASH = @HASH";
+
+                return GetDataTable(sql, new Parameters("HASH", DbType.String, hash));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal int Create(Models.Report report)
+        {
+
+            string sql = @"INSERT INTO `reports` 
+                                (`content`, `creationdate`, 
+                                `patientid`, `appointmentid`, `hash`) 
+                               VALUES
+	                           (@CONTENT,  NOW(), @PATIENTID, @APPOINTMENTID, @HASH)";
+
+            return Execute(sql, new Parameters("CONTENT", DbType.String, report.Content)
+                                   , new Parameters("PATIENTID", DbType.Int32, report.Patient.Nhc)
+                                   , new Parameters("APPOINTMENTID", DbType.String, report.Appointment.Id)
+                                   , new Parameters("HASH", DbType.String, report.Hash)
+                                   );
+
+        }
+
+        internal int Save(Models.Report report)
+        {
+            string sql = @"UPDATE `reports` 
+                               SET 
+                                `content` = @CONTENT, 
+                                `patientid` = @PATIENTID, 
+                                `appointmentid` = @APPOINTMENTID, 
+                                `hash` = @HASH
+                               WHERE `id` = @ID";
+
+            return Execute(sql, new Parameters("ID", DbType.Int32, report.Id)
+                                   , new Parameters("CONTENT", DbType.String, report.Content)
+                                   , new Parameters("PATIENTID", DbType.Int32, report.Patient.Nhc)
+                                   , new Parameters("APPOINTMENTID", DbType.String, report.Appointment.Id)
+                                   , new Parameters("HASH", DbType.String, Leaf.Tools.MD5Tools.GetMd5Hash(report.Content + report.Patient.Nhc))
+                                   );
+        }
+        internal int Sign(Models.Report report)
+        {
+            report.SignDate = DateTime.Now;
+
+            string sql = @"UPDATE `reports` 
+                               SET 
+                                `signdate` = NOW(), 
+                                `hash` = @HASH
+                               WHERE `id` = @ID";
+
+            return Execute(sql, new Parameters("ID", DbType.Int32, report.Id)
+                                   , new Parameters("SIGNDATE", DbType.DateTime, report.SignDate)
+                                   , new Parameters("HASH", DbType.String, Leaf.Tools.MD5Tools.GetMd5Hash(report.Content + report.Patient.Nhc + report.SignDate.ToString("dd/MM/yyyy HH:mm:ss")))
+                                   );
+        }
+
+    }
+}
--- Models/report.model.cs
+++ Models/report.model.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +11,14 @@ namespace Leaf.Models
     {
         private int id;
         private string content;
+        private string hash;
         private DateTime creationDate;
         private DateTime signDate;
         private Patient patient;
         private Appointment appointment;
+
+        private readonly IConfiguration _config;
+
 
         public int Id { get => id; }
         public string Content { get => content; set => content = value; }
@@ -20,5 +26,132 @@ namespace Leaf.Models
         public DateTime SignDate { get => signDate; set => signDate = value; }
         public Patient Patient { get => patient; set => patient = value; }
         public Appointment Appointment { get => appointment; set => appointment = value; }
+        public string Hash { get => hash; set => hash = value; }
+
+        public Report(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Report(int id, IConfiguration config)
+        {
+            _config = config;
+            LoadById(id);
+        }
+
+        public Report(string hash, IConfiguration config)
+        {
+            _config = config;
+            LoadByHash(hash);
+        }
+
+        public Report(Dictionary<string, string> values, IConfiguration config)
+        {
+            _config = config;
+
+            try
+            {
+                this.id = Int32.Parse(values["id"]);
+                LoadById(this.id);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                this.id = -1;
+            }
+
+            this.id = Int32.Parse(values["id"]);
+            Content = values["content"];
+            CreationDate = DateTime.Parse(values["creationDate"]);
+            SignDate = DateTime.Parse(values["signDate"]);
+            Patient = new Patient(Int32.Parse(values["patientid"]), _config);
+            Appointment = new Appointment(Int32.Parse(values["appointmentid"]), _config);
+        }
+
+        private void LoadById(int id)
+        {
+            using (Leaf.Datalayers.Report.DataLayer dl = new Leaf.Datalayers.Report.DataLayer(_config))
+            {
+                System.Data.DataTable dt = dl.loadById(id);
+                if (dt.Rows.Count == 1)
+                {
+                    this.id = Int32.Parse(dt.Rows[0]["id"].ToString());
+                    Content = dt.Rows[0]["content"].ToString();
+                    CreationDate = DateTime.Parse(dt.Rows[0]["creationDate"].ToString());
+                    SignDate = DateTime.Parse(dt.Rows[0]["signDate"].ToString());
+                    Patient = new Patient(Int32.Parse(dt.Rows[0]["patientid"].ToString()), _config);
+                    Appointment = new Appointment(Int32.Parse(dt.Rows[0]["appointmentid"].ToString()), _config);
+                }
+                else
+                {
+                    throw new System.NullReferenceException("Report not found");
+                }
+            }
+        }
+
+        private void LoadByHash(string hash)
+        {
+            using (Leaf.Datalayers.Report.DataLayer dl = new Leaf.Datalayers.Report.DataLayer(_config))
+            {
+                System.Data.DataTable dt = dl.loadByHash(hash);
+                if (dt.Rows.Count == 1)
+                {
+                    this.id = Int32.Parse(dt.Rows[0]["id"].ToString());
+                    Content = dt.Rows[0]["content"].ToString();
+                    CreationDate = DateTime.Parse(dt.Rows[0]["creationDate"].ToString());
+                    SignDate = DateTime.Parse(dt.Rows[0]["signDate"].ToString());
+                    Patient = new Patient(Int32.Parse(dt.Rows[0]["patientid"].ToString()), _config);
+                    Appointment = new Appointment(Int32.Parse(dt.Rows[0]["appointmentid"].ToString()), _config);
+
+                }
+                else
+                {
+                    throw new System.NullReferenceException("Report not found");
+                }
+            }
+        }
+
+        public Report Create()
+        {
+            using (Leaf.Datalayers.Report.DataLayer dl = new Leaf.Datalayers.Report.DataLayer(_config))
+            {
+
+                if (this.Id > 0)
+                {
+                    throw new System.IO.InvalidDataException("Cannot create report with setted Id");
+                }
+
+                if (dl.Create(this) == 1)
+                {
+                    LoadByHash(this.Hash);
+                    return this;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public Report Save()
+        {
+            if (!(this.Id > 0))
+            {
+                throw new System.IO.InvalidDataException("Cannot save report without setted Id");
+            }
+
+            using (Leaf.Datalayers.Report.DataLayer dl = new Leaf.Datalayers.Report.DataLayer(_config))
+            {
+                if (dl.Save(this) == 1)
+                {
+                    LoadById(this.Id);
+                    return this;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
