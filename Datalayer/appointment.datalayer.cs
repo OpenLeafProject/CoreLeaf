8fbfec666153bab94a149e0e8b34372beeef4f9a
Alex Latorre <alex.latorre@ginsa.cat>
2020-07-27 14:35:58 +0200

    Appointment Working model

---
+++ Datalayer/appointment.datalayer.cs
@@ -0,0 +1,106 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Diagnostics;
+
+namespace Leaf.Datalayers.Appointment
+{
+    public class DataLayer : Leaf.Data.DataLayerBaseMySQL
+    {
+
+        public DataLayer(IConfiguration _config)
+            : base(new Data.ConnectionDB(_config).getConnectString())
+        {
+
+        }
+
+        internal DataTable loadById(int id)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM appointments WHERE ID = @ID";
+
+                return GetDataTable(sql, new Parameters("ID", DbType.Int32, id));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal DataTable loadByHash(string hash)
+        {
+            try
+            {
+                string sql = @"SELECT * FROM appointments WHERE hash = @HASH";
+
+                return GetDataTable(sql, new Parameters("HASH", DbType.String, hash));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal int Create(Models.Appointment appointment)
+        {
+
+            string sql = @"INSERT INTO `appointments` 
+                                (`datetime`, `forced`, `allowinvoice`, 
+                                `price`, `duration`, `visitmode`, `note`, 
+                                `patientid`, `visittypeid`, `visitmodeid`,
+                                `hash`) 
+                               VALUES
+	                           (@DATETIME, @FORCED, @ALLOWINVOICE, @PRICE, @DURATION, @VISITMODE, 
+                                @NOTE, @PATIENTID, @VISITTYPEID, @VISITMODEID, @HASH)";
+
+            return Execute(sql, new Parameters("DATETIME", DbType.String, appointment.Datetime)
+                              , new Parameters("FORCED", DbType.String, appointment.Forced)
+                              , new Parameters("ALLOWINVOICE", DbType.String, appointment.AllowInvoice)
+                              , new Parameters("PRICE", DbType.String, appointment.Price)
+                              , new Parameters("DURATION", DbType.String, appointment.Duration)
+                              , new Parameters("VISITMODE", DbType.String, appointment.VisitMode)
+                              , new Parameters("NOTE", DbType.String, appointment.Note)
+                              , new Parameters("PATIENTID", DbType.String, appointment.Patient.Nhc)
+                              , new Parameters("VISITTYPEID", DbType.String, appointment.VisitType.Id)
+                              , new Parameters("VISITMODEID", DbType.String, appointment.VisitMode.Id)
+                              , new Parameters("HASH", DbType.String, appointment.Hash)
+                           );
+
+        }
+
+        internal int Save(Models.Appointment appointment)
+        {
+            string sql = @"UPDATE `appointments` 
+                               SET 
+                                `datetime` = @DATETIME, 
+                                `forced` = @FORCED, 
+                                `allowinvoice` = @ALLOWINVOICE, 
+                                `price` = @PRICE, 
+                                `duration` = @DURATION, 
+                                `visitmode` = @VISITMODE,
+                                `note` = @NOTE,
+                                `patientid` = @PATIENTID,
+                                `visittypeid` = @VISITTYPEID,
+                                `visitmodeid` = @VISITMODEID,
+                                `hash` = @HASH
+                               WHERE `id` = @ID";
+
+            return Execute(sql, new Parameters("ID", DbType.String, appointment.Id)
+                              , new Parameters("DATETIME", DbType.String, appointment.Datetime)
+                              , new Parameters("FORCED", DbType.String, appointment.Forced)
+                              , new Parameters("ALLOWINVOICE", DbType.String, appointment.AllowInvoice)
+                              , new Parameters("PRICE", DbType.String, appointment.Price)
+                              , new Parameters("DURATION", DbType.String, appointment.Duration)
+                              , new Parameters("VISITMODE", DbType.String, appointment.VisitMode)
+                              , new Parameters("NOTE", DbType.String, appointment.Note)
+                              , new Parameters("PATIENTID", DbType.String, appointment.Patient.Nhc)
+                              , new Parameters("VISITTYPEID", DbType.String, appointment.VisitType.Id)
+                              , new Parameters("VISITMODEID", DbType.String, appointment.VisitMode.Id)
+                              , new Parameters("HASH", DbType.String, appointment.Hash)
+                          );
+        }
+    }
+}
--- Models/appointment.model.cs
+++ Models/appointment.model.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,24 +13,168 @@ namespace Leaf.Models
         private DateTime datetime;
         private int forced;
         private int allowInvoice;
-        private int price;
+        private Double price;
         private int duration;
-        private int visitmode;
         private string note;
+        private string hash;
         private Patient patient;
-        private VisiType visitType;
+        private VisitType visitType;
         private VisitMode visitMode;
+
+        private readonly IConfiguration _config;
 
         public int Id { get => id; }
         public DateTime Datetime { get => datetime; set => datetime = value; }
         public int Forced { get => forced; set => forced = value; }
         public int AllowInvoice { get => allowInvoice; set => allowInvoice = value; }
-        public int Price { get => price; set => price = value; }
+        public Double Price { get => price; set => price = value; }
         public int Duration { get => duration; set => duration = value; }
-        public int Visitmode { get => visitmode; set => visitmode = value; }
         public VisitMode VisitMode { get => visitMode; set => visitMode = value; }
         public string Note { get => note; set => note = value; }
         public Patient Patient { get => patient; set => patient = value; }
-        public VisiType VisitType { get => visitType; set => visitType = value; }
+        public VisitType VisitType { get => visitType; set => visitType = value; }
+        public string Hash { get => hash; set => hash = value; }
+
+        public Appointment(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Appointment(int id, IConfiguration config)
+        {
+            _config = config;
+            LoadById(id);
+        }
+
+        public Appointment(string hash, IConfiguration config)
+        {
+            _config = config;
+            LoadByHash(hash);
+        }
+
+        public Appointment(Dictionary<string, string> values, IConfiguration config)
+        {
+            _config = config;
+
+            try
+            {
+                this.id = Int32.Parse(values["id"]);
+                LoadById(this.id);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                this.id = -1;
+            }
+
+            this.id = Int32.Parse(values["id"]);
+            Datetime = DateTime.Parse(values["datetime"]);
+            Forced = Int32.Parse(values["forced"]);
+            AllowInvoice = Int32.Parse(values["allowinvoice"]);
+            Price = Double.Parse(values["price"]);
+            Duration = Int32.Parse(values["duration"]);
+            Note = values["note"];
+            Patient = new Patient(Int32.Parse(values["patientid"]), _config);
+            VisitType = new VisitType(Int32.Parse(values["visittypeid"]), _config);
+            VisitMode = new VisitMode(Int32.Parse(values["visitmodeid"]), _config);
+
+        }
+
+        private void LoadById(int id)
+        {
+            using (Leaf.Datalayers.Appointment.DataLayer dl = new Leaf.Datalayers.Appointment.DataLayer(_config))
+            {
+                System.Data.DataTable dt = dl.loadById(id);
+                if (dt.Rows.Count == 1)
+                {
+                    this.id = Int32.Parse(dt.Rows[0]["id"].ToString());
+                    Datetime = DateTime.Parse(dt.Rows[0]["datetime"].ToString());
+                    Forced = Int32.Parse(dt.Rows[0]["forced"].ToString());
+                    AllowInvoice = Int32.Parse(dt.Rows[0]["allowinvoice"].ToString());
+                    Price = Double.Parse(dt.Rows[0]["price"].ToString());
+                    Duration = Int32.Parse(dt.Rows[0]["duration"].ToString());
+                    Note = dt.Rows[0]["note"].ToString();
+                    Patient = new Patient(Int32.Parse(dt.Rows[0]["patientid"].ToString()), _config);
+                    VisitType = new VisitType(Int32.Parse(dt.Rows[0]["visittypeid"].ToString()), _config);
+                    VisitMode = new VisitMode(Int32.Parse(dt.Rows[0]["visitmodeid"].ToString()), _config);
+
+
+                }
+                else
+                {
+                    throw new System.NullReferenceException("Appointment not found");
+                }
+            }
+        }
+
+        private void LoadByHash(string hash)
+        {
+            using (Leaf.Datalayers.Appointment.DataLayer dl = new Leaf.Datalayers.Appointment.DataLayer(_config))
+            {
+                System.Data.DataTable dt = dl.loadByHash(hash);
+                if (dt.Rows.Count == 1)
+                {
+                    this.id = Int32.Parse(dt.Rows[0]["id"].ToString());
+                    Datetime = DateTime.Parse(dt.Rows[0]["datetime"].ToString());
+                    Forced = Int32.Parse(dt.Rows[0]["forced"].ToString());
+                    AllowInvoice = Int32.Parse(dt.Rows[0]["allowinvoice"].ToString());
+                    Price = Double.Parse(dt.Rows[0]["price"].ToString());
+                    Duration = Int32.Parse(dt.Rows[0]["duration"].ToString());
+                    Note = dt.Rows[0]["note"].ToString();
+                    Patient = new Patient(Int32.Parse(dt.Rows[0]["patientid"].ToString()), _config);
+                    VisitType = new VisitType(Int32.Parse(dt.Rows[0]["visittypeid"].ToString()), _config);
+                    VisitMode = new VisitMode(Int32.Parse(dt.Rows[0]["visitmodeid"].ToString()), _config);
+
+
+                }
+                else
+                {
+                    throw new System.NullReferenceException("Appointment not found");
+                }
+            }
+        }
+
+        public Appointment Create()
+        {
+            using (Leaf.Datalayers.Appointment.DataLayer dl = new Leaf.Datalayers.Appointment.DataLayer(_config))
+            {
+
+                if (this.Id > 0)
+                {
+                    throw new System.IO.InvalidDataException("Cannot create appointment with setted Id");
+                }
+
+                if (dl.Create(this) == 1)
+                {
+                    LoadByHash(this.Hash);
+                    return this;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public Appointment Save()
+        {
+            if (!(this.Id > 0))
+            {
+                throw new System.IO.InvalidDataException("Cannot save appointment without setted Id");
+            }
+
+            using (Leaf.Datalayers.Appointment.DataLayer dl = new Leaf.Datalayers.Appointment.DataLayer(_config))
+            {
+                if (dl.Save(this) == 1)
+                {
+                    LoadById(this.Id);
+                    return this;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
