8cf2799d609697bbcb85b109c865dc2261995419
Alex Latorre <alex.latorre@ginsa.cat>
2020-07-27 14:33:17 +0200

    Apilog Constructor fix

--- Controllers/apilogController.cs
+++ Controllers/apilogController.cs
@@ -12,9 +12,9 @@ namespace Leaf.Controllers
     [ApiController]
     // Enables JWT Auth Security for all methods in controller
     [Leaf.Attributes.HeaderCheckJWT]
-    public class apilogController : ControllerBase
+    public class ApiLogController : ControllerBase
     {
-        private readonly ILogger<patientController> _logger;
+        private readonly ILogger<ApiLogController> _logger;
         private readonly IConfiguration _config;
 
         /// <summary>
@@ -22,7 +22,7 @@ namespace Leaf.Controllers
         /// </summary>
         /// <param name="logger"> Logguer object for debug </param>
         /// <param name="config"> appsettings.json properties </param>
-        public apilogController(ILogger<patientController> logger, IConfiguration config)
+        public ApiLogController(ILogger<ApiLogController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
